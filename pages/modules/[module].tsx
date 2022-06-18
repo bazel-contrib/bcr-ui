855b6cbbc033f6cedae580cf3adec7c59d220f01
Max Goisser <max@field33.com>
2022-06-18 08:56:06 +0200

    Add analyzing of modules via buildozer

--- pages/modules/[module].tsx
+++ pages/modules/[module].tsx
@@ -1,26 +1,36 @@
-import type { GetStaticProps, NextPage } from "next";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { Header, USER_GUIDE_LINK } from "../../components/Header";
-import { Footer } from "../../components/Footer";
+import type { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { Header, USER_GUIDE_LINK } from '../../components/Header'
+import { Footer } from '../../components/Footer'
 import {
+  extractModuleInfo,
   getCompatibilityLevelOfVersion,
   getModuleMetadata,
   getSubmissionCommitOfVersion,
   listModuleNames,
   Metadata,
-} from "../../data/utils";
+  ModuleInfo,
+} from '../../data/utils'
 
 interface ModulePageProps {
-  metadata: Metadata;
-  versionInfos: VersionInfo[];
+  metadata: Metadata
+  versionInfos: VersionInfo[]
 }
 
 const ModulePage: NextPage<ModulePageProps> = ({ metadata, versionInfos }) => {
-  const router = useRouter();
-  const { module } = router.query;
+  const router = useRouter()
+  const { module } = router.query
 
-  const latestVersion = metadata.versions[metadata.versions.length - 1];
+  const latestVersion = metadata.versions[metadata.versions.length - 1]
+  const version = latestVersion
+
+  const versionInfo = versionInfos.find((n) => n.version === version)
+  if (!versionInfo) {
+    throw Error(
+      `Version information for version \`${version}\` of module \`${module}\` could not be retrieved`
+    )
+  }
 
   return (
     <div className="flex flex-col">
@@ -44,7 +54,7 @@ const ModulePage: NextPage<ModulePageProps> = ({ metadata, versionInfos }) => {
                   <p>
                     To start using this module, make sure you have set up Bzlmod
                     according to the <a href={USER_GUIDE_LINK}>user guide</a>,
-                    and add the following to your <code>MODULE.bazel</code>{" "}
+                    and add the following to your <code>MODULE.bazel</code>{' '}
                     file:
                   </p>
                   <div className="p-2 mt-4 rounded bg-gray-200">
@@ -62,7 +72,10 @@ const ModulePage: NextPage<ModulePageProps> = ({ metadata, versionInfos }) => {
                         <div className="rounded-full border h-14 w-14 grid place-items-center">
                           {version.version}
                         </div>
-                        <div>compatibility level {version.compatibilityLevel}</div>
+                        <div>
+                          compatibility level{' '}
+                          {version.moduleInfo.compatibilityLevel}
+                        </div>
                         <a
                           href={`https://github.com/bazelbuild/bazel-central-registry/commit/${version.submission.hash}`}
                           className="ml-auto text-link-color hover:text-link-color-hover"
@@ -71,6 +84,27 @@ const ModulePage: NextPage<ModulePageProps> = ({ metadata, versionInfos }) => {
                         </a>
                       </li>
                     ))}
+                  </ul>
+                </div>
+                <h2 className="text-2xl font-bold mt-4">Dependencies</h2>
+                <div>
+                  <ul className="mt-4">
+                    {versionInfo.moduleInfo.dependencies.map((dependency) => (
+                      <a
+                        key={dependency.module}
+                        href={`/modules/${dependency.module}`}
+                      >
+                        <li className="border rounded p-2 mt-2 flex items-center gap-4">
+                          <div className="rounded-full border h-14 w-14 grid place-items-center">
+                            {dependency.version}
+                          </div>
+                          <div>{dependency.module}</div>
+                        </li>
+                      </a>
+                    ))}
+                    {versionInfo.moduleInfo.dependencies.length === 0 && (
+                      <span>No dependencies</span>
+                    )}
                   </ul>
                 </div>
               </div>
@@ -107,52 +141,52 @@ const ModulePage: NextPage<ModulePageProps> = ({ metadata, versionInfos }) => {
       <div className="flex-grow" />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
 interface VersionInfo {
-  version: string;
+  version: string
   submission: {
-    hash: string;
-    authorDate: string;
-    authorDateRel: string;
-  };
-  compatibilityLevel: number,
+    hash: string
+    authorDate: string
+    authorDateRel: string
+  }
+  moduleInfo: ModuleInfo
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { module } = params as any;
-  const metadata = await getModuleMetadata(module);
+  const { module } = params as any
+  const metadata = await getModuleMetadata(module)
 
-  const versionInfos = await Promise.all(
+  const versionInfos: VersionInfo[] = await Promise.all(
     metadata.versions.map(async (version) => ({
       version,
       submission: await getSubmissionCommitOfVersion(module, version),
-      compatibilityLevel: await getCompatibilityLevelOfVersion(module, version)
+      moduleInfo: await extractModuleInfo(module, version),
     }))
-  );
+  )
 
   return {
     props: {
       metadata,
       versionInfos,
     },
-  };
-};
+  }
+}
 
 export async function getStaticPaths() {
-  const modulesNames = await listModuleNames();
+  const modulesNames = await listModuleNames()
 
   const paths = modulesNames.map((name) => ({
     params: { module: name },
-  }));
+  }))
 
   return {
     paths,
     // TODO: fallback true?
     fallback: false,
     // fallback: true, false or "blocking" // See the "fallback" section below
-  };
+  }
 }
 
-export default ModulePage;
+export default ModulePage
