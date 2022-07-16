9f7f13b4b2120f6c64b54b284a41879b241273c3
Max Goisser <max@field33.com>
2022-07-16 14:31:57 +0200

    Manual deploy

---
+++ _next/static/chunks/pages/modules/[module]-87a5810a4495932a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[129],{8435:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/modules/[module]",function(){return _(6378)}])}},function(n){n.O(0,[253,687,152,354,378,774,888,179],(function(){return u=8435,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
