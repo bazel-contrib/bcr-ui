bccc0b8d12d92f4d0cf21e366b1cc34f4e78556e
Hannes Käufler <hanneskaeufler@users.noreply.github.com>
2023-01-07 00:41:11 +0100

    Add text when not finding any search results (#43)
    Closes https://github.com/bazel-contrib/bcr-ui/issues/34

--- pages/search.tsx
+++ pages/search.tsx
@@ -13,6 +13,9 @@ interface SearchPageProps {
   searchIndex: SearchIndexEntry[]
 }
 
+const GITHUB_ISSUE_LINK =
+  'https://github.com/bazelbuild/bazel-central-registry/issues/new?assignees=&labels=module+wanted&template=module_wanted.yaml&title=wanted%3A+%5Bgithub+path+of+the+module%2C+e.g.+bazelbuild%2Frules_foo%5D'
+
 const Search: NextPage<SearchPageProps> = ({ searchIndex }) => {
   const [searchResults, setSearchResults] = useState<SearchIndexEntry[]>([])
 
@@ -86,9 +89,28 @@ const Search: NextPage<SearchPageProps> = ({ searchIndex }) => {
           <div className="w-full max-w-4xl">
             <h2 className="font-bold text-lg">Search results</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
-              {searchResults.map(({ module, version }) => (
-                <ModuleCard key={module} {...{ module, version }} />
-              ))}
+              {searchResults && searchResults.length ? (
+                searchResults.map(({ module, version }) => (
+                  <ModuleCard key={module} {...{ module, version }} />
+                ))
+              ) : (
+                <div className="text-gray-600">
+                  <p>
+                    No results for "
+                    <span className="text-black">{router.query.q}</span>".
+                  </p>
+                  <p>
+                    You can{' '}
+                    <a
+                      href={GITHUB_ISSUE_LINK}
+                      className="text-link-color hover:text-link-color-hover"
+                    >
+                      open an issue on GitHub
+                    </a>{' '}
+                    to request the module.
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </div>
