e6232eba1dabfc42113614672ee63172839b7f6f
hobofan <goisser94@gmail.com>
2023-08-23 08:43:43 +0000

    Deploying to gh-pages from @ bazel-contrib/bcr-ui@a5f10c5612b23adbf4068a30d8605c80c2305f0f 🚀

---
+++ _next/static/xlxwFJ4XAcinvCQVN5zyi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fall-modules","\u002Fmodules\u002F[module]","\u002Fmodules\u002F[module]\u002F[version]","\u002Fsearch"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
