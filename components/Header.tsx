025201d7bf2d0d0a4830fb257f375bf5a4c2485f
Maximilian Goisser <goisser94@gmail.com>
2023-10-25 21:07:42 +0200

    Adjust NavBar to have working search on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,16 +11,10 @@ export const CONTRIBUTE_CTA_LINK =
 export const Header: React.FC<HeaderProps> = () => {
   const router = useRouter()
   const [searchQueryInput, setSearchQueryInput] = useState<string>('')
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<bool>(false)
 
-  const handleSearchKeydown = (
-    event: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (event.code === 'Enter') {
-      handleSubmitSearch()
-    }
-  }
-
-  const handleSubmitSearch = () => {
+  const handleSubmitSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     router.push({
       pathname: '/search',
       query: { ...router.query, q: searchQueryInput },
@@ -29,7 +23,7 @@ export const Header: React.FC<HeaderProps> = () => {
 
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 bg-bzl-green">
-      <div className="container flex flex-wrap items-center mx-auto">
+      <div className="container flex flex-wrap gap-4 flex-col sm:flex-row items-center mx-auto">
         <div className="flex flex-1 items-center">
           <Link href="/" className="flex items-center cursor-pointer">
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white cursor-pointer">
@@ -38,27 +32,7 @@ export const Header: React.FC<HeaderProps> = () => {
           </Link>
         </div>
         <div className="flex flex-1 gap-4 items-center md:order-1 transform md:translate-x-1/4">
-          <button
-            type="button"
-            data-collapse-toggle="mobile-menu-3"
-            aria-controls="mobile-menu-3"
-            aria-expanded="false"
-            className="md:hidden text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm p-2.5 mr-1"
-          >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div className="hidden relative md:block">
+          <div className="block relative">
             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
               <svg
                 className="w-5 h-5 text-gray-500"
@@ -73,28 +47,29 @@ export const Header: React.FC<HeaderProps> = () => {
                 ></path>
               </svg>
             </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Search for module..."
-              onChange={(e) => setSearchQueryInput(e.target.value)}
-              onKeyDown={handleSearchKeydown}
-              onSubmit={handleSubmitSearch}
-            />
+            <form onSubmit={handleSubmitSearch} className="contents">
+              <input
+                type="text"
+                id="search-navbar"
+                className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search for module..."
+                onChange={(e) => setSearchQueryInput(e.target.value)}
+              />
+            </form>
           </div>
           <Link
             href={'/all-modules'}
-            className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 md:p-0"
+            className="hidden sm:block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 md:p-0"
           >
             Browse all modules
           </Link>
           <button
             data-collapse-toggle="mobile-menu-3"
             type="button"
-            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            className="hidden inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             aria-controls="mobile-menu-3"
             aria-expanded="false"
+            onClick={() => setMobileMenuOpen(!setMobileMenuOpen)}
           >
             <svg
               className="w-6 h-6"
@@ -127,7 +102,7 @@ export const Header: React.FC<HeaderProps> = () => {
           id="mobile-menu-3"
         >
           <div className="relative mt-3 md:hidden">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none text-white">
               <svg
                 className="w-5 h-5 text-gray-500"
                 fill="currentColor"
@@ -141,15 +116,15 @@ export const Header: React.FC<HeaderProps> = () => {
                 ></path>
               </svg>
             </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Search..."
-              onChange={(e) => setSearchQueryInput(e.target.value)}
-              onKeyDown={handleSearchKeydown}
-              onSubmit={handleSubmitSearch}
-            />
+            <form onSubmit={handleSubmitSearch} className="contents">
+              <input
+                type="text"
+                id="search-navbar-mobile"
+                className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search..."
+                onChange={(e) => setSearchQueryInput(e.target.value)}
+              />
+            </form>
           </div>
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
