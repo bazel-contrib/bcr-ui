37ae72823ee3e3e1127ba1a9e51bfd762524f200
Xùdōng Yáng <wyverald@gmail.com>
2024-08-07 15:04:12 -0400

    fiiiiiinnnne i'll install pnpm

--- data/moduleStaticProps.ts
+++ data/moduleStaticProps.ts
@@ -107,17 +107,17 @@ function composeCmps<T>(...cmps: Cmp<T>[]): Cmp<T> {
 }
 
 const compareIdentifiers: Cmp<string> = composeCmps(
-  comparing((id) => !/^\d+$/.test(id)),  // pure numbers compare less than non-numbers
+  comparing((id) => !/^\d+$/.test(id)), // pure numbers compare less than non-numbers
   comparing((id) => (/^\d+$/.test(id) ? parseInt(id) : 0)),
   natural
 )
 
 const compareVersions: Cmp<Version> = composeCmps(
   comparing((v) => v.release, lexicographically(compareIdentifiers)),
-  comparing((v) => v.prerelease.length === 0),  // nonempty prerelease compares less than empty prerelease
+  comparing((v) => v.prerelease.length === 0), // nonempty prerelease compares less than empty prerelease
   comparing((v) => v.prerelease, lexicographically(compareIdentifiers))
 )
-  
+
 const sortVersions = (versions: string[]): string[] => {
   const parsed = versions.map(parseVersion)
   parsed.sort(compareVersions)
