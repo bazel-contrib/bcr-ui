c53b0d71c5571cf4e2d76db0e4c2991f8c36acf2
Grzegorz Lukasik <32371993+hauserx@users.noreply.github.com>
2025-01-22 18:40:21 +0100

    chore: run prettier --write . (#155)
    Ups, I have created https://github.com/bazel-contrib/bcr-ui/pull/154 from
    github UI, and no pre-submit checks were executed.

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   basePath: '',
   // Increase timeout for generating static pages from default 60s to avoid issues like:
   // Restarted static page generation for /modules/xxx/y.y.y because it took more than 60 seconds
-  staticPageGenerationTimeout: 600
+  staticPageGenerationTimeout: 600,
 }
 
 module.exports = nextConfig
