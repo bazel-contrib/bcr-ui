13dbc8b0a501c2b347776f699760566f7236a96b
Grzegorz Lukasik <32371993+hauserx@users.noreply.github.com>
2025-01-22 16:36:03 +0100

    Increase static page generation timeout 60s -> 600s (#154)

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   // for hosting under GitHub pages
   basePath: '',
+  // Increase timeout for generating static pages from default 60s to avoid issues like:
+  // Restarted static page generation for /modules/xxx/y.y.y because it took more than 60 seconds
+  staticPageGenerationTimeout: 600
 }
 
 module.exports = nextConfig
