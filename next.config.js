67f5d2c416a4b36f50c4555d03724076e72e7122
Alex Eagle <alex@aspect.dev>
2025-08-19 01:43:34 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   // for hosting under GitHub pages
-  basePath: '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   // Increase timeout for generating static pages from default 60s to avoid issues like:
   // Restarted static page generation for /modules/xxx/y.y.y because it took more than 60 seconds
   staticPageGenerationTimeout: 600,
