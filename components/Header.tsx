4520609cf7b451d606c6f5f3e16e2427be7d213e
Alex Eagle <alex@aspect.dev>
2025-08-22 16:27:12 -0700

    remove duplicate magnifying glass

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,20 +42,6 @@ export const Header: React.FC<HeaderProps> = () => {
         </div>
         <div className="flex flex-1 gap-4 items-center md:order-1 transform md:translate-x-1/4">
           <div className="block relative">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
             <div id="docsearch" />
           </div>
           <Link
