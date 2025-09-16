f4cc24c70508ac2e1a3f88dc1f7049eb15d67f56
alexeagle <alex@aspect.build>
2025-09-16 15:49:02 +0000

    Deploying to gh-pages from @ bazel-contrib/bcr-ui@bbb324bdedf22fd92704690d23904bf053041c8e 🚀

---
+++ _next/static/chunks/pages/modules/[module]/[version]-0489802fbd55a794.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[145],{1639:(_,e,u)=>{"use strict";u.r(e),u.d(e,{__N_SSG:()=>n,default:()=>d});var s=u(9054),n=!0;let d=s.default},6984:(_,e,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/modules/[module]/[version]",function(){return u(1639)}])}},_=>{_.O(0,[217,968,57,255,54,636,593,792],()=>_(_.s=6984)),_N_E=_.O()}]);
