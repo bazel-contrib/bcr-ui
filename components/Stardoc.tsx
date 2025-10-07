7531df9c084df1a969398808448401b91aa19ee1
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-10-07 16:51:51 +0200

    Handle rule attributes 14 and 15 from stardoc_output proto
    The buf proto for stardoc outputs is outdated and doesn't handle newer attribute types, including a attr.string_keyed_label_dict (https://bazel.build/rules/lib/toplevel/attr#string_keyed_label_dict).
    This change is a workaround to prevent the doc generation from failing, but isn't a permanent fix.
    
    Closes https://github.com/bazel-contrib/bcr-ui/issues/207

--- components/Stardoc.tsx
+++ components/Stardoc.tsx
@@ -11,6 +11,15 @@ import { AttributeType } from '@buf/bazel_bazel.bufbuild_es/src/main/java/com/go
 
 // port of https://github.com/bazelbuild/bazel/blob/09c621e4cf5b968f4c6cdf905ab142d5961f9ddc/src/main/java/com/google/devtools/build/skydoc/rendering/MarkdownUtil.java#L248-L282
 function attributeTypeDescription(attributeType: number): string {
+  // special handling for types that are missing from the outdated @buf/bazel_bazel.bufbuild_es
+  // See also: https://github.com/bazel-contrib/bcr-ui/issues/207
+  if (attributeType == 14) {
+    return 'dictionary: String → Label'
+  }
+  if (attributeType == 15) {
+    return 'dictionary: String → list of labels'
+  }
+
   switch (AttributeType[attributeType]) {
     case 'NAME':
       return 'name'
