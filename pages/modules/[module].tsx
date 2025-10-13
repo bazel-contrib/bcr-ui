bcbb9548011ae684a86a9b3bd8b7ab9babcc8d7a
Alex Eagle <alex@aspect.dev>
2025-10-12 19:45:01 -0700

    break out Dependencies component

--- pages/modules/[module].tsx
+++ pages/modules/[module].tsx
@@ -1,6 +1,5 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Header, USER_GUIDE_LINK } from '../../components/Header'
 import { Footer } from '../../components/Footer'
@@ -18,11 +17,11 @@ import {
   VersionInfo,
 } from '../../data/moduleStaticProps'
 import { GithubRepositoryMetadata } from '../../data/githubMetadata'
-import { formatDistance, parseISO } from 'date-fns'
 import { faGlobe, faScaleBalanced } from '@fortawesome/free-solid-svg-icons'
 import { StardocRenderer } from '../../components/Stardoc'
 import { LeftNavComponent } from '../../components/LeftNavComponent'
 import { VersionHistory } from '../../components/VersionHistory'
+import { Dependencies } from '../../components/Dependencies'
 
 interface ModulePageProps {
   metadata: Metadata
@@ -355,131 +354,18 @@ const ModulePage: NextPage<ModulePageProps> = ({
                       deprecated={deprecated}
                       metadata={metadata}
                     />
-                    <div className="mt-4">
-                      <h2 id="dependencies" className="text-2xl font-bold mt-4">
-                        Dependency graph
-                      </h2>
-                    </div>
-                    <div className="mt-4">
-                      <details open>
-                        <summary>
-                          <span
-                            role="heading"
-                            aria-level={2}
-                            className="text-1xl mt-4"
-                          >
-                            <span className="font-bold">Direct</span> (
-                            {versionInfo.moduleInfo.dependencies.filter(
-                              (d) => !d.dev
-                            ).length || 'None'}
-                            ){' '}
-                            <small className="text-sm font-normal text-gray-500">
-                              at version {selectedVersion}
-                            </small>
-                          </span>
-                        </summary>
-                        <ul className="mt-4">
-                          {versionInfo.moduleInfo.dependencies
-                            .filter((d) => !d.dev)
-                            .map((dependency) => (
-                              <Link
-                                key={dependency.module}
-                                href={`/modules/${dependency.module}/${dependency.version}`}
-                              >
-                                <li className="p-2 mt-2 flex items-center gap-4 hover:border-gray-800">
-                                  <div className="rounded-full border h-14 w-14 grid place-items-center">
-                                    {dependency.version}
-                                  </div>
-                                  <div>{dependency.module}</div>
-                                </li>
-                              </Link>
-                            ))}
-                          {versionInfo.moduleInfo.dependencies.filter(
-                            (d) => !d.dev
-                          ).length === 0 && <span>No dependencies</span>}
-                        </ul>
-                      </details>
-                    </div>
-                    <div className="mt-4">
-                      <details>
-                        <summary>
-                          <span
-                            role="heading"
-                            aria-level={2}
-                            className="text-1xl mt-4"
-                          >
-                            <span className="font-bold">Dev Dependencies</span>{' '}
-                            (
-                            {versionInfo.moduleInfo.dependencies.filter(
-                              (d) => d.dev
-                            ).length || 'None'}
-                            ){' '}
-                          </span>
-                        </summary>
-                        <ul className="mt-4">
-                          {versionInfo.moduleInfo.dependencies
-                            .filter((d) => d.dev)
-                            .map((dependency) => (
-                              <Link
-                                key={dependency.module}
-                                href={`/modules/${dependency.module}/${dependency.version}`}
-                              >
-                                <li className="p-2 mt-2 flex items-center gap-4 hover:border-gray-800">
-                                  <div className="rounded-full border h-14 w-14 grid place-items-center">
-                                    {dependency.version}
-                                  </div>
-                                  <div>{dependency.module}</div>
-                                </li>
-                              </Link>
-                            ))}
-                          {versionInfo.moduleInfo.dependencies.length === 0 && (
-                            <span>No dependencies</span>
-                          )}
-                        </ul>
-                      </details>
-                    </div>
-                    <div className="mt-4">
-                      <details>
-                        <summary>
-                          <span
-                            role="heading"
-                            aria-level={2}
-                            className="text-1xl mt-4"
-                          >
-                            <span className="font-bold">Dependents</span>{' '}
-                            {reverseDependencies.length > 0
-                              ? `(${reverseDependencies.length})`
-                              : ''}
-                          </span>
-                        </summary>
-                        <ul className="mt-4">
-                          {shownReverseDependencies.map((revDependency) => (
-                            <Link
-                              key={revDependency}
-                              href={`/modules/${revDependency}`}
-                            >
-                              <li className="p-2 mt-2 flex items-center gap-4 hover:border-gray-800">
-                                <div>{revDependency}</div>
-                              </li>
-                            </Link>
-                          ))}
-                          {reverseDependencies.length === 0 && (
-                            <span>No dependent modules yet</span>
-                          )}
-                        </ul>
-                        {displayShowAllReverseDependenciesButton && (
-                          <button
-                            className="font-semibold p-2 mt-4 w-full hover:shadow-lg"
-                            onClick={() =>
-                              setTriggeredShowAllReverseDependencies(true)
-                            }
-                          >
-                            Show all {reverseDependencies.length} dependent
-                            modules
-                          </button>
-                        )}
-                      </details>
-                    </div>
+                    <Dependencies
+                      versionInfo={versionInfo}
+                      selectedVersion={selectedVersion}
+                      reverseDependencies={reverseDependencies}
+                      shownReverseDependencies={shownReverseDependencies}
+                      displayShowAllReverseDependenciesButton={
+                        displayShowAllReverseDependenciesButton
+                      }
+                      setTriggeredShowAllReverseDependencies={
+                        setTriggeredShowAllReverseDependencies
+                      }
+                    />
                   </div>
                 </div>
               </div>
