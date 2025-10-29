5882f1395513eabc91f30a62eba934a5f156c5c4
Alex Eagle <alex@aspect.dev>
2025-10-29 10:13:37 -0700

    chore: gather fundingLinks for GH repos
    Will use this to show the 'sponsor this module' badge

--- pages/docs/[module].tsx
+++ pages/docs/[module].tsx
@@ -71,9 +71,10 @@ const DocsPage: NextPage<DocsPageProps> = ({
                 </h2>
                 <p className="text-gray-500">
                   This module does not publish Starlark API documentation yet.
-                  <br/>
-                  Module authors: you can add documentaton. Read the instructions at:
-                  <br/>
+                  <br />
+                  Module authors: you can add documentaton. Read the
+                  instructions at:
+                  <br />
                   <a href="https://github.com/bazelbuild/bazel-central-registry/blob/main/docs/stardoc.md">
                     https://github.com/bazelbuild/bazel-central-registry/blob/main/docs/stardoc.md
                   </a>
