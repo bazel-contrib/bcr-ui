732c87b1f6eb3d0c49418544382f398b6ccbe13d
Alex Eagle <alex@aspect.dev>
2025-11-09 22:17:50 -0500

    move boost.json route out of the way
    Next.js dynamic routes snapshot their static props into [route].json, and there is now a 'boost' module

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   // Increase timeout for generating static pages from default 60s to avoid issues like:
   // Restarted static page generation for /modules/xxx/y.y.y because it took more than 60 seconds
   staticPageGenerationTimeout: 600,
+
+  exportPathMap: async function (defaultPathMap) {
+    const newMap = { ...defaultPathMap }
+    Object.keys(newMap).forEach((path) => {
+      if (path.includes('boost.json')) {
+        newMap[path.replace('boost.json', 'boost-json')] = newMap[path]
+        delete newMap[path]
+      }
+    })
+    return newMap
+  },
 }
 
 module.exports = nextConfig
